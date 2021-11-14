e582793740256a1fef62fd3d4ceaa74667c05fc2
Antony Ventura <antonyventuramorey@gmail.com>
2021-11-14 09:42:04 -0400

    React Native Vecto icons working properly - Android configuration only

--- src/pages/LoadingScreen.tsx
+++ src/pages/LoadingScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon  from "react-native-vector-icons/Ionicons";
 
 export const LoadingScreen = () => {
     return (
         <View>
             <Text>Loading Screen</Text>
+
+            <Icon
+                name="star-outline"
+                color="red"
+                size={ 110 }
+            />
         </View>
     )
 }
--- src/pages/LoginScreen.tsx
+++ src/pages/LoginScreen.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon  from "react-native-vector-icons/Ionicons";
 
 export const LoginScreen = () => {
     return (
         <View>
         <Text>Login Screen</Text>  
+
+        <Icon
+                name="star-outline"
+                color="red"
+                size={ 110 }
+            />
         </View>
     )
 }
